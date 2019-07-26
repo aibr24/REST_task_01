bf2b7aa363cc1bddcb5bc0decd9a3fae3a37e7c1
ali <aliibrahim4793@gmail.com>
2019-07-26 05:32:32 +0300

    rest task 1 commit

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,16 @@
-from .models import Flight, Booking
+from .models import *
+from .serializer import *
+from rest_framework.generics import ListAPIView
+from django.shortcuts import render
+
+
+
+
+class Flights(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightSerializer
+
+
+class Bookings(ListAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
